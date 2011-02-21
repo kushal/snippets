f9c6fcbadc2d5175ad544530807efce7787738ec
Kushal Dave <kushal@foursquare.com>
2011-02-21 01:08:11 -0500

    Linkify in web UI and fix quoted text detection.

--- receive_email.py
+++ receive_email.py
@@ -3,8 +3,8 @@ import email
 import logging
 import re
 
-from google.appengine.ext import webapp 
-from google.appengine.ext.webapp.mail_handlers import InboundMailHandler 
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.ext.webapp import util
 
 from dateutil import date_for_new_snippet
@@ -14,18 +14,18 @@ class ReceiveEmail(InboundMailHandler):
     """Receive a snippet email and create or replace snippet for this week."""
 
     def receive(self, message):
-        user = user_from_email(email.utils.parseaddr(message.sender)[1])               
+        user = user_from_email(email.utils.parseaddr(message.sender)[1])
         for content_type, body in message.bodies('text/plain'):
             content = body.decode()
-            
+
             sig_pattern = re.compile(r'^\-\-\s*$', re.MULTILINE)
             split_email = re.split(sig_pattern, content)
             content = split_email[0]
-            
-            reply_pattern = re.compile(r'^On.*snippets.*wrote:$', re.MULTILINE)
+
+            reply_pattern = re.compile(r'^On.*foursquare.com.*wrote:$', re.MULTILINE)
             split_email = re.split(reply_pattern, content)
             content = split_email[0]
-            
+
             create_or_replace_snippet(user, content, date_for_new_snippet())
 
 
@@ -35,5 +35,5 @@ def main():
 
 if __name__ == '__main__':
     main()
-        
+
 
